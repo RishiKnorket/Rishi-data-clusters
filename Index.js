151421e9dbb71d09aa443912ca45e3fc0576e851
RishiKnorket <151186775+RishiKnorket@users.noreply.github.com>
2024-04-15 13:23:24 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,482 @@
+const jsonData = {
+  "snowflakes": {
+    "Database type 1": {
+      "knorket":
+      {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    },
+    "Database type 1.1": {
+      "knorket": {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    }
+    // ... Add more entries as needed
+  },
+  "aws": {
+    "Database type 1": {
+      "knorket":
+      {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    },
+    "Database type 1.1": {
+      "knorket": {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    }
+    // ... Add more entries as needed
+  },
+  "aw1s": {
+    "Database type 1": {
+      "knorket":
+      {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    },
+    "Database type 1.1": {
+      "knorket": {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    }
+    // ... Add more entries as needed
+  },
+  "a2w1s": {
+    "Database type 1": {
+      "knorket":
+      {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    },
+    "Database type 1.1": {
+      "knorket": {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    }
+    // ... Add more entries as needed
+  },
+  "a2w21s": {
+    "Database type 1": {
+      "knorket":
+      {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    },
+    "Database type 1.1": {
+      "knorket": {
+        "auth": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      },
+      "connect": {
+        "Public": {},
+        "information_schema": {
+          "applicable_roles": [
+            ["grantee", "varchar"],
+            ["grantee_type", "varchar"],
+            ["role_name", "varchar"],
+            ["is_grantable", "varchar"]
+          ],
+          "columns": [
+            ["table_catalog", "varchar"],
+            ["table_schema", "varchar"],
+            ["table_name", "varchar"],
+            ["column_name", "varchar"],
+            ["ordinal_position", "bigint"],
+            ["column_default", "varchar"],
+            ["is_nullable", "varchar"],
+            ["data_type", "varchar"]
+          ],
+        },
+        "vault": {}
+      }
+    }
+    // ... Add more entries as needed
+  },
+};
